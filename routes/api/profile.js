17f6c873cdefcd4f0dcde31ebc5e4726df68222d
Louis-Ramirez <Louis.Ramirez60@myhunter.cuny.edu>
2020-03-31 14:09:44 -0400

    Delete Education, experiences and account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 
 //req=request, res=response
@@ -150,7 +151,8 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
-    //@todo - remove users posts
+    //Remove users posts
+    await Post.deleteMany({ user: req.user.id });
 
     //Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
